4b8c52086cd0d22ef1951334fecf0107979b526d
Dawid <dawlyc1995@gmail.com>
2020-12-29 01:32:16 +0100

    Update store.js

--- server/store.js
+++ server/store.js
@@ -1,5 +1,11 @@
 import {store} from 'react-redux';
+import {travel, user} from 'react-reducers';
 
 const reduxStore = store.init();
+
+const reducers = combinerReducer(
+  travel, 
+  user
+);
 
 console.log(reduxStore);
